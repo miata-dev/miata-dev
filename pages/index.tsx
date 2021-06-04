a770cca32618b501d5607b0642c9bdf7e7652d51
karniv00l <piotr.rogowski0@gmail.com>
2021-06-04 09:18:47 +0200

    Fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Miata.dev</title>
+        <title>miata.dev</title>
         <meta name="description" content="Mazda Miata MX5 DIY stuff" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Coming soon!
